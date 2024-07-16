cb1923a8775c9f73394261fb047ca129a511f40c
DevRamona <r.ingabire@alustudent.com>
2024-07-16 23:11:21 +0200

    declarative

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-ReactDOM.render(<h1>Hello, everyone! </h1>, document.getElementById("root"))
+// // ReactDOM.render(<ul> <li>Names</li><li>Clases</li> </ul>
+// //     , document.getElementById("root"))
+
+// ReactDOM.render(<h1 class="header">Head</h1>, document.appendChild("#root"))
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is imperative"
+h1.className = "header"
+document.getElementById("root").append(h1)
