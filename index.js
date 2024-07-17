b8a8dbb8d665ae0702309106516331abab74e970
DevRamona <r.ingabire@alustudent.com>
2024-07-17 11:29:37 +0200

    using JSX

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 // // ReactDOM.render(<ul> <li>Names</li><li>Clases</li> </ul>
 // //     , document.getElementById("root"))
 
-// ReactDOM.render(<h1 class="header">Head</h1>, document.appendChild("#root"))
+// const elements = (
+// <div>
+//     <h1 className="header">This is JSX</h1>
+//     <p>First heading</p>
+// </div>
+// )
+// console.log(elements)
+//  ReactDOM.render(elements, document.getElementById("root"))
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is imperative"
-h1.className = "header"
-document.getElementById("root").append(h1)
+// // const h1 = document.createElement("h1")
+// h1.textContent = "This is imperative"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+const navBar = (
+<nav>
+    <h1>website</h1>
+    <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+</nav>
+) 
+
+
+ReactDOM.render(navBar, document.getElementById("root"))
